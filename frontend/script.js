422ee4fda2ac9e9528686f173856cb1015e2a50c
Zennasa <samuele.zennaro@itiszuccante.edu.it>
2025-04-05 10:42:52 +0200

    frontend

---
+++ frontend/script.js
@@ -0,0 +1,41 @@
+
+function fetchData(){
+    fetch("../fetchHandler.php")
+        .then(response => {
+            return response.json();
+        })
+        .then(json => {
+            let data = document.getElementById('generalDataContainer');
+
+            data.innerHTML = '';
+
+            for(var i in json){
+                let row = json[i];
+                data.innerHTML += 
+                `
+                <div id="data">
+                <div id="dataContent">
+                        ${row['Name']}
+                </div>
+                </div>
+                `;
+            }
+            
+        })
+}
+
+
+function prenotare() {
+    var btn1 = document.getElementById('btn1');
+    if(btn1.innerHTML == "PRENOTA") {
+        btn1.innerHTML = "SONO TORNATO";
+        btn1.style.backgroundColor = '#DC143C';
+        btn1.style.color = 'white';
+    } else {
+        btn1.innerHTML = "PRENOTA";
+        btn1.style.backgroundColor = '#60F360';
+        btn1.style.color = 'black';
+    }
+
+    
+}
