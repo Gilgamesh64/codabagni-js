b7cd4b33f8eb9106714f39d97789861f1af5010f
Zennasa <samuele.zennaro@itiszuccante.edu.it>
2025-04-08 10:42:40 +0200

    LOGIN PAGE

--- php/loginPage.php
+++ php/loginPage.php
@@ -5,24 +5,25 @@
         background-color: lightskyblue;
         text-align: center;
         justify-items: center;
+        user-select: none;
     }
-    
-    #titoloContainer {
-    display: flex;
-    justify-content: center;
-}
 
-#titolo {
-    color: white;
-    text-shadow: 2px 3px 0px black;
-    font-weight: bolder;
-    font-size: 72px;
-}
+    #titoloContainer {
+        display: flex;
+        justify-content: center;
+    }
+
+    #titolo {
+        color: white;
+        text-shadow: 2px 3px 0px black;
+        font-weight: bolder;
+        font-size: 72px;
+    }
 
     form {
         background-color: cornflowerblue;
         color: white;
-        zoom: 200%;
+        zoom: 300%;
         width: fit-content;
         border: 5px solid blue;
         border-radius: 25px;
@@ -33,32 +34,48 @@
     }
 
     input {
+        user-select: auto;
         border: 2px solid blue;
+        background-color: lightgreen;
+        border-radius: 25px;
+        padding: 5px;
+        transition: 0.3s;
+        outline: none;
     }
-    input:valid {
-        background-color: azure;
+    
+    input:focus {
+        border-color: blue;
     }
-    input:invalid {
-        background-color: aquamarine;
+    input:not(:placeholder-shown):invalid {
+        background-color: lightcoral;
+        border-color: red;
+        transition: 0.3s;
     }
 
     #login,
     #register {
+        width: 45%;
         background-color: aquamarine;
-        border: 3px solid blue;
+        border: 2px solid blue;
         border-radius: 100px;
         margin-top: 2px;
         margin-inline: 2px;
     }
+
+    #login:active,
+    #register:active {
+        background-color: blue;
+        color: white;
+    }
 </style>
 <div id="titoloContainer">
     <div id="titolo">Coda Bagni</div>
-</div>  
+</div>
 <form method="post">
-                <label>User</label><br>
-                <input type="text" name="userID" pattern="^\S+$" required /><br><br>
-                <label>Password</label><br>
-                <input type="password" name="userPassword" pattern="^\S+$" required /><br><br>
+    <label>User</label><br>
+    <input type="text" name="userID" pattern="^\S+$" placeholder="Name" title="Name with no spaces." required /><br><br>
+    <label>Password</label><br>
+    <input type="password" name="userPassword" pattern="^\S+$" placeholder="Password" title="Password with no spaces." required /><br><br>
     <button type="submit" name="submit" id="login" value="login">Login</button>
     <button type="submit" name="submit" id="register" value="register">Register</button>
 </form>
